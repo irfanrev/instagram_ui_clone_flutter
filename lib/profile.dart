62d184d107a58646aca99720740d508b092894a8
Irfan Maulana <irfan.resimen2018@gmail.com>
2021-10-29 22:54:14 +0700

    Profile setengahnya

--- lib/profile.dart
+++ lib/profile.dart
@@ -33,26 +33,242 @@ class Profile extends StatelessWidget {
       ),
       body: ListView(
         children: [
+          SizedBox(
+            height: 10.0,
+          ),
           Container(
+            padding: EdgeInsets.symmetric(horizontal: 15),
             width: MediaQuery.of(context).size.width,
-            height: 150,
-            color: Colors.grey,
+            height: 115,
             child: Row(
               children: [
+                Container(
+                  width: 115,
+                  height: 115,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[100],
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                      color: Colors.grey,
+                      width: 3.0,
+                    ),
+                    image: DecorationImage(
+                      image: AssetImage('assets/avatar.jpg'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 10.0,
+                ),
                 Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Container(
-                      width: 100,
-                      height: 100,
-                      decoration: BoxDecoration(
-                        color: Colors.grey[100],
-                        shape: BoxShape.circle,
+                    Text(
+                      '100',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
                       ),
-                    )
+                    ),
+                    Text(
+                      'Postingan',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
                   ],
+                ),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      '100',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      'Pengikut',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      '100',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      'Mengikuti',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            width: MediaQuery.of(context).size.width,
+            height: 100,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Irfan Maulana',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+                Text(
+                  'Software Development',
+                  style: TextStyle(color: Colors.grey),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                    'Lorem Ipsum is simply dummy text of the printing and typesetting industry')
+              ],
+            ),
+          ),
+          Spotlight(),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            child: GridView.count(
+              crossAxisCount: 3,
+              children: [
+                Container(
+                  color: Colors.grey,
+                ),
+                Container(
+                  color: Colors.grey,
+                ),
+                Container(
+                  color: Colors.grey,
                 )
               ],
             ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Spotlight extends StatelessWidget {
+  const Spotlight({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      padding: EdgeInsets.only(left: 15),
+      height: 110,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: [
+          Column(
+            children: [
+              Container(
+                width: 80,
+                height: 80,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.grey[200],
+                ),
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              Text('Koding')
+            ],
+          ),
+          SizedBox(
+            width: 15.0,
+          ),
+          Column(
+            children: [
+              Container(
+                width: 80,
+                height: 80,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.grey[200],
+                ),
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              Text('Flutter')
+            ],
+          ),
+          SizedBox(
+            width: 15.0,
+          ),
+          Column(
+            children: [
+              Container(
+                width: 80,
+                height: 80,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.grey[200],
+                ),
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              Text('School')
+            ],
+          ),
+          SizedBox(
+            width: 15.0,
+          ),
+          Column(
+            children: [
+              Container(
+                width: 80,
+                height: 80,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(width: 2.0),
+                ),
+                child: Center(
+                  child: Icon(
+                    Icons.add,
+                    size: 30,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              Text('Baru')
+            ],
           ),
         ],
       ),
