27666f7016757e0bffbeae31903dcfe9dc67285d
Irfan Maulana <irfan.resimen2018@gmail.com>
2021-10-29 20:50:21 +0700

    profile started

--- lib/profile.dart
+++ lib/profile.dart
@@ -5,6 +5,57 @@ class Profile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 1.0,
+        backgroundColor: Colors.white,
+        title: Text(
+          'irfan.rev',
+          style: TextStyle(color: Colors.black),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.add_box_outlined,
+              color: Colors.black,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.menu_rounded,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+      body: ListView(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 150,
+            color: Colors.grey,
+            child: Row(
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        color: Colors.grey[100],
+                        shape: BoxShape.circle,
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
